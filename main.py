70502d8e224cb056c4990b27a300b21997b2010b
Arnaud-Intel <arnaud.marti@outlook.fr>
2026-02-26 22:09:05 +0100

    It's rotating!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import *
 from logger import log_state
+from player import *
 
 def main():
 
@@ -9,18 +10,26 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
-        pygame.init()
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        dt = game_clock.tick(60)/1000
 
 
 
